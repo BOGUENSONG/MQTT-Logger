f3305ae12481d6adfdb7f47430830d1106a9f944
boguensong <idsonic95@gmail.com>
2019-12-24 17:16:16 +0900

    주석

--- master/receiver.py
+++ master/receiver.py
@@ -2,29 +2,30 @@ import paho.mqtt.client as mqtt
 import datetime
 
 
-
+# 서버에 연결 되었을 때 실행되는 함수
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("connect")
     else:
         print("Bad connection")
 
+# message가 도착했을 때 실행되는 함수
 def on_message(client, userdata, msg):
-    logmsg = str(msg.payload.decode('utf-8'))
+    logmsg = str(msg.payload.decode('utf-8')) #로그메시지
 
-    idName = msg.topic.replace("log/","")
-    filename = "/app/usb/" + logmsg[0:10]+ "_" + idName + ".txt"
+    idName = msg.topic.replace("log/","") # 메시지 보낸 장치의 토픽id
+    filename = "/app/usb/" + logmsg[0:10]+ "_" + idName + ".txt" # 저장할 파일명 YYYY-MM-DD_id#.txt의 형식
     logfile = open(filename,'a')
     logfile.write(logmsg + "\n")
     logfile.close()
 
-    print(logmsg)
+    print(logmsg) #화면에도 로그메세지 출력
 
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+client = mqtt.Client() #클라이언트 설정
+client.on_connect = on_connect #콜백함수 설정 (연결완료시)
+client.on_message = on_message #콜백함수 설정 (메시지도착시)
 
-client.connect('log-server.local', 1883)
-client.subscribe('log/#',1)
-client.loop_forever()
+client.connect('log-server.local', 1883) # 서버에 연결
+client.subscribe('log/#',1) # 토픽명이 log/로 시작하는모든 것들을 구독한다.
+client.loop_forever() #계속 반복한다.
--- slave/sender.py
+++ slave/sender.py
@@ -4,32 +4,35 @@ import datetime
 import pytz
 import time
 
+#broker (서버)의 주소와 포트번호 MQTT는 1883이 기본이다.
 broker_address ='log-server.local'
 broker_port = 1883
 
+# 토픽 설정과 bitrate설정
 ser = serial.Serial('/dev/ttyACM0',115200)
+
+# 서버와 연결시 실행되는 함수
 def on_connect(client, userdata, flags, rc):
     print("연결이 완료되었습니다")
+
+# 현재 시간을 ISO-8601에 맞추어 반환.
 def clock():
     utc_offset_sec = time.altzone if time.localtime().tm_isdst else time.timezone
     utc_offset = datetime.timedelta(seconds=-utc_offset_sec)
     
     return datetime.datetime.now().replace(tzinfo=datetime.timezone(offset=utc_offset)).isoformat()
-#def on_publish(client, userdata, mid)
-  #  print()
 
 
-client = mqtt.Client()
-client.on_connect = on_connect
-#client.on_publish = on_publish
+client = mqtt.Client() #클라이언트 설정
+client.on_connect = on_connect #connect시 콜백함수 설정
 
-client.connect(host=broker_address, port=broker_port)
-client.loop_start()
-while 1:
-    logline = ser.readline()
-    timestamp = clock()
-    log = timestamp + " / "+ str(logline)
-    print(log)
-    client.publish("log/id1",log)
+client.connect(host=broker_address, port=broker_port) #서버와 연결
+client.loop_start() #반복 시작
+while 1: 
+    logline = ser.readline() #장치로부터 오는 메시지 (로그)를 읽는다.
+    timestamp = clock() #현재 시간을 받는다.
+    log = timestamp + " / "+ str(logline) #메시지 모양은 현재시간 / 장치로그 로 설정.
+    print(log) # 화면에 로그모양을 출력해준다.
+    client.publish("log/id1",log) # log/내 id의 토픽으로 로그를 보낸다.
     
 
