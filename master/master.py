44921743b51df44eb82457ba449877c242f644b3
boguensong <idsonic95@gmail.com>
2019-12-26 12:43:03 +0900

    file name & remark modify

---
+++ master/master.py
@@ -0,0 +1,31 @@
+import paho.mqtt.client as mqtt
+import datetime
+
+
+# if server connect , call this function
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("connect")
+    else:
+        print("Bad connection")
+
+# if on message, call this function
+def on_message(client, userdata, msg):
+    logmsg = str(msg.payload.decode('utf-8')) # logmessage from topic
+    folderName = "/app/usb/" #location for save
+    idName = msg.topic.replace("log/","") # message sender id
+    filename = folderName + logmsg[0:10]+ "_" + idName + ".txt" # folder/YYYY-MM-DD_id#.txt [saving format]
+    logfile = open(filename,'a') # open file
+    logfile.write(logmsg + "\n") # write log
+    logfile.close() # close file
+
+    print(logmsg) # logmesesage print for debugging
+
+
+client = mqtt.Client() #client config
+client.on_connect = on_connect # callback function config (on_connect)
+client.on_message = on_message # callback function config (on_message)
+
+client.connect('log-server.local', 1883) # connect server ('servername', port)
+client.subscribe('log/#',1) # subscribe all 'topic#'
+client.loop_forever() # loop forever
---
+++ slave/slave.py
@@ -0,0 +1,39 @@
+import serial
+import paho.mqtt.client as mqtt
+import datetime
+import pytz
+import time
+
+#server address & port , mqtt default port 1883
+broker_address ='log-server.local'
+broker_port = 1883
+
+# serial communication config (device,bitrate)
+ser = serial.Serial('/dev/ttyACM0',115200) 
+
+# on_connect callback function
+def on_connect(client, userdata, flags, rc):
+    print("연결이 완료되었습니다")
+
+# Formatting function for log time format
+def clock():
+    utc_offset_sec = time.altzone if time.localtime().tm_isdst else time.timezone
+    utc_offset = datetime.timedelta(seconds=-utc_offset_sec)
+    
+    return datetime.datetime.now().replace(tzinfo=datetime.timezone(offset=utc_offset)).isoformat()
+
+
+client = mqtt.Client() #client config
+client.on_connect = on_connect # on_connect callback function config
+
+client.connect(host=broker_address, port=broker_port) #server conncet
+
+client.loop_start() #loop start
+while 1: 
+    logline = ser.readline() #read log from device
+    timestamp = clock() # get present time
+    log = timestamp + " / "+ str(logline) # log message fotmat [ timestamp / logmsg]
+    print(log) # print for debug
+    client.publish("log/id1",log) # publish to "topic", log
+    
+
