1085ec78feac0c9d498cf232b6611966d818ba84
BOGUENSONG <idsonic95@gmail.com>
2020-01-06 02:12:12 +0000

    change program.py and bin file

--- master/program.py
+++ master/program.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import argparse
+import time
 
 serverIP = 'log-server.local'
 serverPort = 1883 
@@ -12,7 +13,30 @@ args = parser.parse_args()
 
 deviceID = str(args.n)
 filename = str(args.b)
-dest = "command/" + deviceID + "/program/bin"
+topic1 = "command/" + deviceID + "/nolja/bin"
+topic2 = "command/" + deviceID + "/kflash/bit_mic"
+topic3 = "command/" + deviceID + "/kflash/dan"
+numb = 0
+while True:
+    print("##### select topic ###############")
+    print(" 1. " + topic1)
+    print(" 2. " + topic2)
+    print(" 3. " + topic3)
+    print("##################################")
+    print("number : ",end="")
+    numb = input()
+    print("##################################")
+    if (numb is "1"):
+        dest = topic1
+        break
+    elif (numb is "2"):
+        dest = topic2
+        break
+    elif (numb is "3"):
+        dest = topic3
+        break
+    else:
+        print("\nWrong Number Choose another one\n")
 # if server connect, call this function
 def on_connect(client, userdata, flags, rc):
     print("server connect success")
@@ -27,8 +51,7 @@ client.on_publish = on_publish #call back func
 client.connect('log-server.local',1883) # connect to server
 f = open(filename,'rb')
 data = f.read()
-client.publish(dest,data) # send message to topic
 f.close()
+client.publish(dest,filename,qos=0)
+client.publish(dest,data,qos=1)
 client.disconnect() #discconect server
-
-
